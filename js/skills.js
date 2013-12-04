1b279b2ddceef0f8b0f75c42591ad72625754304
KatherineMichel <kthrnmichel@gmail.com>
2013-12-04 01:31:19 -0600

    Update skills.js

--- js/skills.js
+++ js/skills.js
@@ -1,7 +1,8 @@
 var skills = [" Beautiful, Responsive, Web Development",
 " Project Management",
 " Social Media Platforms",
+" Forefront Technology Trends",
 " Mobile App Development",
-" Artistry and Design",
+" Artistry, Music and Design",
 " Solutions Architecture",
 " Networking and Infrastructure"];
