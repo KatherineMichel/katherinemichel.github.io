61f2fac23c4719523835f0ae856e53a7bbf189ac
KatherineMichel <kthrnmichel@gmail.com>
2013-12-03 18:34:31 -0600

    Create skills.js

---
+++ js/skills.js
@@ -0,0 +1,7 @@
+var skills = [" Beautiful, Responsive, Web Development",
+" Project Management",
+" Social Media",
+" Mobile Apps",
+" Artistry and Design",
+" Solutions Architecture",
+" Networking and Infrastructure"];
