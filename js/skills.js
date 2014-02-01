d21b47bac6bb1729a3b635c5ea85d0bad8c2f067
Katherine Michel <kthrnmichel@gmail.com>
2014-02-01 02:59:45 -0600

    Moving files to another location

--- js/skills.js
+++
@@ -1,8 +0,0 @@
-var skills = [" Beautiful, Responsive, Web Development",
-" Project Management",
-" Social Media Platforms",
-" Latest Technology Trends",
-" Mobile App Development",
-" Artistry, Music, and Design",
-" Solutions Architecture",
-" Networking and Infrastructure"];
