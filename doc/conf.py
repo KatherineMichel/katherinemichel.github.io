92a7ee80be036d45b989f8fd1e771a149635e7d1
Katherine "Kati" Michel <kthrnmichel@gmail.com>
2025-08-02 13:07:22 -0500

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -25,6 +25,8 @@ extensions = [
 myst_update_mathjax = False
 
 ogp_site_url = 'https://katherinemichel.github.io/'
+ogp_description_length = 300
+ogp_type = "article"
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
