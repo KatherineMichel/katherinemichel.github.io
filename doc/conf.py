98f985298ab547c302d0f7a822d3da55302ac4e4
Katherine "Kati" Michel <kthrnmichel@gmail.com>
2025-08-02 19:26:25 -0500

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -45,7 +45,8 @@ post_show_prev_next = True
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+# html_static_path = ['_static']
+html_static_path = []
 
 # Use 'blog' to apply sidebar to blog only
 html_sidebars = {
