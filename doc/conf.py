e89004910f2e1d83491311b8a92e52660697c1cf
Katherine "Kati" Michel <kthrnmichel@gmail.com>
2025-08-02 19:21:50 -0500

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -47,19 +47,19 @@ post_show_prev_next = True
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-# # Use 'blog' to apply sidebar to blog only
-# html_sidebars = {
-#    '**': [
-#       'about.html',
-#       'searchfield.html',
-#       'ablog/postcard.html',
-#       'ablog/navigation.html',
-#       'ablog/recentposts.html',
-#       'ablog/archives.html',
-#       'ablog/tagcloud.html',
-#       'ablog/categories.html',
-#     ]
-# }
+# Use 'blog' to apply sidebar to blog only
+html_sidebars = {
+   '**': [
+      'about.html',
+      'searchfield.html',
+      'ablog/postcard.html',
+      'ablog/navigation.html',
+      'ablog/recentposts.html',
+      'ablog/archives.html',
+      'ablog/tagcloud.html',
+      'ablog/categories.html',
+    ]
+}
 
 # html_sidebars = {
 #    "**": [
